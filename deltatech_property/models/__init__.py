c3c82999ebc0b2ce1c0d70284f74b62056b7acd9
Borni DHIFI <dhifi.borni@gmail.com>
2020-11-21 14:09:16 +0100

    [ADD] app deltatech_property

---
+++ deltatech_property/models/__init__.py
@@ -0,0 +1,5 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from . import property_abstract, property_building, property_config, property_land, property_room, res_partner
